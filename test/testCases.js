555171118c32ae74b19029b1e2c4305bcbdbe2d7
Jimmy Jose <jimmymadappallil@gmail.com>
2012-12-09 23:10:50 +0530

    more test cases added for test2

--- test/testCases.js
+++ test/testCases.js
@@ -27,27 +27,48 @@ var getStringTest = new YAHOO.tool.TestCase({
 		name: getStringTest,
 	
 		testNumber : function(){
-			var result = getString('3');	
+			var result = getString('3', 1);	
 			YAHOO.util.Assert.areEqual('ERROR! Expects a number', result, "Expects a number");
 		},
 
 		testFizzBuzz : function(){
-			var result = getString(15);
+			var result = getString(15, 1);
 			YAHOO.util.Assert.areEqual('FizzBuzz', result, "should return 'FizzBuzz'");
 		},
 
 		testFizz : function(){
-			var result = getString(3);
+			var result = getString(3, 1);
 			YAHOO.util.Assert.areEqual('Fizz', result, "should return 'Fizz'");
 		},
 
 		testBuzz : function(){
-			var result = getString(5);
+			var result = getString(5, 1);
 			YAHOO.util.Assert.areEqual('Buzz', result, "should return 'Buzz'");
 		},
 
+		testFizz_BuzzBazz : function(){
+			var result = getString(11, 1);
+			YAHOO.util.Assert.areEqual('Bazz', result, "should return 'Bazz'");
+		},
+
+		testBuzz_FizzBazz : function(){
+			var result = getString(7, 1);
+			YAHOO.util.Assert.areEqual('Bazz', result, "should return 'Bazz'");
+		},
+
+		testFizzBuzzBazz : function(){
+			var result = getString(16, 1);
+			YAHOO.util.Assert.areEqual('Bazz', result, "should return 'Bazz'");
+		},
+
+		testFizzBuzzBazz : function(){
+			var result = getString(16, 0);
+			YAHOO.util.Assert.areEqual(16, result, "should return 16");
+		},
+
+
 		testOriginal : function(){
-			var result = getString(2);	
+			var result = getString(2, 1);	
 			YAHOO.util.Assert.areEqual(2, result, "should return the original num");
 		}
 		
