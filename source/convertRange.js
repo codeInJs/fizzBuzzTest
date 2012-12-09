b8f241ab6fd82334635270bc123be57630a51b0e
Jimmy Jose <jimmymadappallil@gmail.com>
2012-12-09 21:31:52 +0530

    two empty functions convertRange and getString added

---
+++ source/convertRange.js
@@ -0,0 +1,25 @@
+/* convert a range to strings 'Fizz', 'Buzz'
+	rule 1 : if number divisible by 3 -> output 'Fizz'
+	rule 2 : if number divisible by 5 -> ouput 'Buzz'
+	rule 3 : if number divisible by both 3 and 5 -> output 'FizzBuzz'
+*/
+
+/*
+	convert a range to 'fizz/buzz/number' as per the abouve rules
+	@param range<Array>
+	returns <String>
+*/
+
+function convertRange(range){
+
+}
+
+/*	returns Fizz/Buzz/num for a given number
+	@param num <Number>
+	return <String / Number>	
+*/
+
+function getString(num){
+
+
+}
