ca36064d62d88b5c4e8c0892589477031099a637
Jimmy Jose <jimmymadappallil@gmail.com>
2012-12-09 21:49:25 +0530

    check for Fizz, Buzz, number

--- source/convertRange.js
+++ source/convertRange.js
@@ -42,6 +42,12 @@ function getString(num){
 
 	if(num % 3 == 0 && num % 5 == 0){
 		return "FizzBuzz";
+	}else if(num % 3 == 0 ){
+		return 'Fizz'; 
+	}else if(num % 5 == 0){
+		return 'Buzz';
+	}else{
+		return num;
 	}
 
 }
